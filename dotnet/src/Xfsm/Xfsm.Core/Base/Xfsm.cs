1c2c1ee117a71c31406dba38347753820c4426da
gio-js <g.sorgente86@gmail.com>
2024-04-21 22:42:27 +0200

    test project

--- dotnet/src/Xfsm/Xfsm.Core/Base/Xfsm.cs
+++ dotnet/src/Xfsm/Xfsm.Core/Base/Xfsm.cs
@@ -38,17 +38,40 @@ namespace Xfsm.Core.Base
             this.endingStates.Add(endingState);
         }
 
+        /// <summary>
+        /// The method will check every initialization on database has been done successfully.
+        /// The state machine will run successfully based on data point of view.
+        /// </summary>
         public abstract void EnsureInitialized();
 
+        /// <summary>
+        /// In order to initialize an external data source (statically or dinamically) you can use this method
+        /// in order to retrieve the right scripts to run against your database.
+        /// </summary>
+        /// <returns></returns>
         public abstract string RetrieveDDLScript();
 
+        /// <summary>
+        /// Returns the state machine fetching mode (basically a queue or a stack)
+        /// </summary>
+        /// <returns></returns>
         public XfsmFetchMode GetFetchMode()
         {
             return this.fetchMode;
         }
 
+        /// <summary>
+        /// Retrieve a single element from the bag (if any) using the fetch mode
+        /// </summary>
+        /// <param name="fetchState"></param>
+        /// <returns></returns>
         public abstract IXfsmElement<T> Fetch(IXfsmState fetchState);
 
+        /// <summary>
+        /// Add a new element to the bag
+        /// </summary>
+        /// <param name="businessElement"></param>
+        /// <param name="elementState"></param>
         public abstract void AddElement(T businessElement, IXfsmState elementState);
     }
 }

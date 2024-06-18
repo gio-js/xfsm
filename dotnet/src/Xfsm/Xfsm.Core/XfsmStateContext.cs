7772a542fd7e8d5bd36467e125666f28f3fec9e3
gio-js <g.sorgente86@gmail.com>
2024-06-18 17:12:53 +0200

    StateContext and related factory

---
+++ dotnet/src/Xfsm/Xfsm.Core/XfsmStateContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xfsm.Core.Interfaces;
+
+namespace Xfsm.Core
+{
+    /// <summary>
+    /// State context represents the actor in charge of execution of business logic
+    /// </summary>
+    public class XfsmStateContext<T> : IXfsmStateContext
+    {
+        private IXfsmBag<T> xfsmInstance;
+        private IXfsmState<T> state;
+        private IXfsmElement<T> element;
+
+        /// <summary>
+        /// Creates the state context
+        /// </summary>
+        /// <param name="element"></param>
+        public XfsmStateContext(IXfsmBag<T> xfsmInstance, IXfsmState<T> state, IXfsmElement<T> element)
+        {
+            this.xfsmInstance = xfsmInstance;
+            this.element = element;
+        }
+
+        /// <summary>
+        /// Executes the related business logic of current state
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        internal void Execute()
+        {
+            state.Execute(element.GetBusinessElement(), this);
+        }
+
+        /// <summary>
+        /// Changes the state of specified element
+        /// </summary>
+        /// <param name="state"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void ChangeState(Enum state)
+        {
+            // TODO: should be transactional
+            xfsmInstance.Done(element);
+            xfsmInstance.AddElement(element.GetBusinessElement(), state);
+        }
+    }
+}
---
+++ dotnet/src/Xfsm/Xfsm.Core/XfsmStateContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xfsm.Core.Interfaces;
+
+namespace Xfsm.Core
+{
+    /// <summary>
+    /// State context represents the actor in charge of execution of business logic
+    /// </summary>
+    public class XfsmStateContextFactory<T> : IXfsmStateContextFactory<T>
+    {
+        private IXfsmBag<T> xfsmBag;
+        private IXfsmState<T> state;
+
+        public XfsmStateContextFactory(IXfsmBag<T> xfsmBag, IXfsmState<T> state) { }
+
+        /// <summary>
+        /// Executes the related business logic of current state
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public IXfsmStateContext Create(IXfsmElement<T> element)
+        {
+            return new XfsmStateContext<T>(xfsmBag, state, element);
+        }
+    }
+}

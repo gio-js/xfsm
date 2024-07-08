b6674c5dba4502b6ea0e081bd5129afa008ff1ae
gio-js <g.sorgente86@gmail.com>
2024-07-08 23:49:33 +0200

    added java scaffolding project

---
+++ java/xfsm.core/src/main/java/com/giojs/xfsm/XfsmApplication.java
@@ -0,0 +1,13 @@
+package com.giojs.xfsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XfsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XfsmApplication.class, args);
+	}
+
+}
---
+++ java/xfsm.core/src/test/java/com/giojs/xfsm/XfsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giojs.xfsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XfsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
